be8925791936027ab11930b4e09e453ddd6f487a
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-13 14:36:01 -0300

    First commit

---
+++ src/main/java/com/dev/dino/demoparkapi/UdemyDemoParkApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.dino.demoparkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyDemoParkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyDemoParkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/dino/demoparkapi/UdemyDemoParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.dino.demoparkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyDemoParkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
