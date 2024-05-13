708d5844629d21eae844925a73705f0f02a593af
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-13 17:19:26 -0300

    Post de usuario pt.1

--- src/main/java/com/dev/dino/demoparkapi/controllers/UsuarioController.java
+++ src/main/java/com/dev/dino/demoparkapi/controllers/UsuarioController.java
@@ -1,7 +1,12 @@
 package com.dev.dino.demoparkapi.controllers;
 
+import com.dev.dino.demoparkapi.entity.Usuario;
 import com.dev.dino.demoparkapi.services.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario obj) {
+        Usuario user = usuarioService.create(obj);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 }
--- src/main/java/com/dev/dino/demoparkapi/services/UsuarioService.java
+++ src/main/java/com/dev/dino/demoparkapi/services/UsuarioService.java
@@ -1,12 +1,19 @@
 package com.dev.dino.demoparkapi.services;
 
+import com.dev.dino.demoparkapi.entity.Usuario;
 import com.dev.dino.demoparkapi.repositories.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario create(Usuario obj) {
+        return usuarioRepository.save(obj);
+    }
 }
