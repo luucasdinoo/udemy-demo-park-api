566a8678de10e8dd30e39a891877b750e794f5a8
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-14 14:35:15 -0300

    getById e update password

--- src/main/java/com/dev/dino/demoparkapi/controllers/UsuarioController.java
+++ src/main/java/com/dev/dino/demoparkapi/controllers/UsuarioController.java
@@ -5,10 +5,7 @@ import com.dev.dino.demoparkapi.services.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController @RequestMapping("api/v1/usuarios")
 @RequiredArgsConstructor
@@ -20,5 +17,17 @@ public class UsuarioController {
     public ResponseEntity<Usuario> create(@RequestBody Usuario obj) {
         Usuario user = usuarioService.create(obj);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario obj) {
+        Usuario user = usuarioService.editarSenha(id, obj.getPassword());
+        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/dev/dino/demoparkapi/services/UsuarioService.java
+++ src/main/java/com/dev/dino/demoparkapi/services/UsuarioService.java
@@ -16,4 +16,14 @@ public class UsuarioService {
     public Usuario create(Usuario obj) {
         return usuarioRepository.save(obj);
     }
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+    }
+    @Transactional
+    public Usuario editarSenha(Long id, String password) {
+        Usuario user = buscarPorId(id);
+        user.setPassword(password);
+        return user;
+    }
 }
